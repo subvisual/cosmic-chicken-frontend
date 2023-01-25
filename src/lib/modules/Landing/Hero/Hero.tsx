a55fc12d3f30f0ca6e9a42541ee1ccc4d2a3e90c
David Lange <lange@finiam.com>
2023-01-25 16:06:53 +0000

    responsive fixes

--- src/lib/modules/Landing/Hero/Hero.tsx
+++ src/lib/modules/Landing/Hero/Hero.tsx
@@ -4,7 +4,7 @@ import styles from "./Hero.module.css";
 export default function Hero() {
   return (
     <section className={styles["root"]}>
-      <div className="relative flex-1 flex items-end justify-center mb-9">
+      <div className="relative flex-1 flex items-end justify-center 2xl:mb-9">
         <Image
           src="/images/asteroid-2.png"
           alt="An asteroid"
@@ -12,29 +12,22 @@ export default function Hero() {
           height={126}
           className={styles["asteroid-2"]}
         />
-        <Image
-          src="/images/asteroid-1.png"
-          alt="Another asteroid"
-          width={254}
-          height={236}
-          className={styles["asteroid-1"]}
-        />
-        <Image
-          src="/images/astronaut.png"
-          alt="An astronaut"
-          width={254}
-          height={457}
-          className={styles["astronaut"]}          
-        />
-        <Image
-          src="/images/egg-box.png"
-          alt="A box of Filecoin eggs"
-          width={304}
-          height={227}
-          className={styles["eggs"]}
-        />
+        <div className={styles["asteroid-1"]}>
+          <Image
+            src="/images/asteroid-1.png"
+            alt="Another asteroid"
+            width={254}
+            height={236}
+          />
+        </div>
+        <div className={styles["astronaut"]}>
+          <Image src="/images/astronaut.png" alt="An astronaut" fill />
+        </div>
+        <div className={styles["eggs"]}>
+          <Image src="/images/egg-box.png" alt="A box of Filecoin eggs" fill />
+        </div>
       </div>
-      <p className="text-center text-large max-w-lg mx-auto text-brown">
+      <p className="text-center text-large  max-w-2xl 2xl:max-w-lg mx-auto text-brown">
         Borrow collateral from reputable lenders and lock the future income
         until the storage providers have repaid their loan.
       </p>
--- src/lib/modules/Layout/LandingHeader.tsx
+++ src/lib/modules/Layout/LandingHeader.tsx
@@ -3,7 +3,7 @@ import Link from "next/link";
 
 export default function LandingHeader() {
   return (
-    <header className="mt-20 mb-8">
+    <header className="mt-8 mb-4 2xl:mt-20 2xl:mb-8">
       <Link href="/">
         <Image
           src="/images/logo.svg"
