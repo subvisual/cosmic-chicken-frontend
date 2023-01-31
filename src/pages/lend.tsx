69b94a66146824dfe039784eae60fa9c1ec7fc05
Paula Carneiro <paulacarneiro215@gmail.com>
2023-01-31 09:33:47 +0000

    display nft data on lender page

--- src/pages/lend.tsx
+++ src/pages/lend.tsx
@@ -1,10 +1,29 @@
 import Mint from "@/lib/components/Mint";
+import { useQuery } from "react-query";
+import { useAccount } from "wagmi";
 
 export default function Lend() {
+  const { address } = useAccount();
+
+  const { data }: { data?: nftData } = useQuery(
+    ["nfts"],
+    async () => await (await fetch("/api/nfts/" + address?.toLowerCase())).json(),
+    { enabled: !!address }
+  );
+
   return (
     <>
       <h1>Lend</h1>
       <Mint />
+      {data?.length && (
+        <ul>
+          {data
+            .sort((a, b) => Number(a.projection_id) - Number(b.projection_id))
+            .map(item => (
+              <li key={item.projection_id}>Chicken Bond #{item.projection_id}</li>
+            ))}
+        </ul>
+      )}
     </>
   );
 }
