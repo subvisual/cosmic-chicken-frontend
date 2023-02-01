b775a0071489907dcad503669d8bcd7766bf7ac5
Paula Carneiro <paulacarneiro215@gmail.com>
2023-02-01 10:06:10 +0000

    Query data  (#1)
    * create endpoints
    
    * install react query and create client
    
    * display nft data on lender page
    
    * refetch every 10 seconds
    
    * add address to query key

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -2,11 +2,18 @@ import "@/styles/globals.css";
 import client from "@/wagmi/client";
 import type { AppProps } from "next/app";
 import { WagmiConfig } from "wagmi";
+import { QueryClient, QueryClientProvider } from "react-query";
+import { ReactQueryDevtools } from "react-query/devtools";
+
+const queryClient = new QueryClient();
 
 export default function App({ Component, pageProps }: AppProps) {
   return (
     <WagmiConfig client={client}>
-      <Component {...pageProps} />
+      <QueryClientProvider client={queryClient}>
+        <Component {...pageProps} />
+        <ReactQueryDevtools />
+      </QueryClientProvider>
     </WagmiConfig>
   );
 }
--- src/pages/lend.tsx
+++ src/pages/lend.tsx
@@ -1,10 +1,32 @@
 import Mint from "@/lib/components/Mint";
+import { useQuery } from "react-query";
+import { useAccount } from "wagmi";
 
 export default function Lend() {
+  const { address } = useAccount();
+
+  const { data }: { data?: nftData } = useQuery(
+    ["nfts", address],
+    async () => await (await fetch("/api/nfts/" + address?.toLowerCase())).json(),
+    {
+      enabled: !!address,
+      refetchInterval: 10000,
+    }
+  );
+
   return (
     <>
       <h1>Lend</h1>
       <Mint />
+      {data?.length && (
+        <ul>
+          {data
+            .sort((a, b) => Number(a.projection_id) - Number(b.projection_id))
+            .map(item => (
+              <li key={item.projection_id}>Chicken Bond #{item.projection_id}</li>
+            ))}
+        </ul>
+      )}
     </>
   );
 }
