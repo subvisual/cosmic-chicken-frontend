a51ce62ca59dcd78f75467f08750a2cc478de69a
Paula Carneiro <paulacarneiro215@gmail.com>
2023-02-03 16:48:29 +0000

    update ui

--- src/lib/components/NftCard.tsx
+++ src/lib/components/NftCard.tsx
@@ -1,49 +1,57 @@
 import { NftMockType } from "../data/nftDataMock";
 import Image from "next/image";
 import useModal from "../hooks/useModal";
+import { truncateAmount } from "../utils/truncate";
 
 export default function NftCard({ nft }: { nft: NftMockType }) {
   const { renderModal } = useModal();
 
   function handleChickenIn() {
     renderModal({
-      message:
-        "By chickening in, you lose your initial deposit and get the boosted token",
+      message: "By chickening in, you lose your initial deposit and get the boosted token",
       onContinue: () => console.log("Get insidee"),
     });
   }
 
   function handleChickenOut() {
     renderModal({
-      message:
-        "By chickening out, you lose your initial deposit and get the boosted token",
+      message: "By chickening out, you lose your initial deposit and get the boosted token",
       onContinue: () => console.log("Get me out"),
     });
   }
 
   return (
     <div className="flex gap-12 rounded-3xl bg-beige drop-shadow-light p-12 pt-14">
-      <Image
-        src={`/images/bonds/bond-${nft.status}.svg`}
-        alt=""
-        width={142.26}
-        height={208.64}
-      />
+      <div className="flex flex-col gap-4">
+        <Image src={`/images/bonds/bond-${nft.status}.svg`} alt="" width={142.26} height={208.64} />
+        <div className="text-center">
+          <p className="leading-none">APY</p>
+          <p className="font-bold">
+            {truncateAmount(
+              (parseFloat(nft.accrued_value.value) / parseFloat(nft.deposit.value)) * 100,
+              2
+            )}
+            %
+          </p>
+        </div>
+      </div>
       <div className="w-full flex flex-col justify-center items-center">
-        <div className="flex justify-around flex-wrap gap-8 py-12 w-full">
+        <div className="flex justify-around flex-wrap gap-8 py-12 w-full text-center">
           <div>
             <p>Created at</p>
-            <p>
+            <p className="font-bold text-xl">
               {new Date(parseInt(nft.created_at)).toLocaleDateString("en-UK")}
             </p>
           </div>
           <div>
             <p>Deposit</p>
-            <p>{nft.deposit.value + " " + nft.deposit.token}</p>
+            <p className="font-bold text-xl">{nft.deposit.value + " " + nft.deposit.token}</p>
           </div>
           <div>
             <p>Accrued value</p>
-            <p>{nft.accrued_value.value + " " + nft.accrued_value.token}</p>
+            <p className="font-bold text-xl">
+              {nft.accrued_value.value + " " + nft.accrued_value.token}
+            </p>
           </div>
         </div>
         {nft.status === "bonding" && (
