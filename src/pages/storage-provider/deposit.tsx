1877c173596bf6b4a070ab0b1af573fbcf44cc83
Paula Carneiro <paulacarneiro215@gmail.com>
2023-02-03 11:50:40 +0000

    deposit to pool contract

--- src/pages/storage-provider/deposit.tsx
+++ src/pages/storage-provider/deposit.tsx
@@ -1,18 +1,41 @@
-import AppHeader from "@/lib/components/AppHeader";
 import Router from "next/router";
-import { ChangeEvent, useState } from "react";
+import { ChangeEvent, useEffect, useState } from "react";
+import { usePrepareContractWrite, useContractWrite, useWaitForTransaction } from "wagmi";
+import { BigNumber } from "ethers";
+import { parseEther } from "ethers/lib/utils.js";
+import AppHeader from "@/lib/components/AppHeader";
+import abi from "../../lib/abi/pool.json";
+import { POOL_CONTRACT } from "../../lib/constants";
 
 export default function Deposit() {
-  const [amount, setAmount] = useState<number>();
+  const [amount, setAmount] = useState<number>(0);
+
+  const { config } = usePrepareContractWrite({
+    address: POOL_CONTRACT,
+    abi,
+    functionName: "depositStorageProvider",
+    overrides: {
+      ...(amount && { value: parseEther(amount?.toString()) }),
+    },
+  });
+
+  const { data, write } = useContractWrite(config);
+
+  const { isLoading } = useWaitForTransaction({
+    hash: data?.hash,
+  });
+
+  useEffect(() => {
+    isLoading && Router.push("/storage-provider");
+  }, [isLoading]);
 
   const handleInputChange = (ev: ChangeEvent<HTMLInputElement>) => {
-    setAmount(parseInt(ev.target.value));
+    setAmount(parseFloat(ev.target.value));
   };
 
   const handleFormSubmit = (ev: ChangeEvent<HTMLFormElement>) => {
     ev.preventDefault();
-    console.log("deposit " + amount);
-    Router.push("/storage-provider");
+    write?.();
   };
 
   return (
@@ -26,7 +49,8 @@ export default function Deposit() {
           <p>Deposit amount</p>
           <input
             required
-            className="text-4xl border border-light-brown rounded-xl py-1 px-3 block max-w-full"
+            min="1"
+            className="text-4xl border border-light-brown rounded-xl py-1 px-3 block max-w-full appearance-none"
             placeholder="100 tFIL"
             onChange={handleInputChange}
           />
