d1c19ef352625f113ce70370ed0cdce209b706c9
Davide Silva <davidesilva@hey.com>
2023-02-09 17:30:10 +0000

    Use env var for indexer url (#20)
    * Use env var for indexer url
    
    Why:
    * We want to be able to change the indexer we use in runtime
    
    How:
    * Using an environment variable to set the INDEXER_URL value
    * Adding a `env.sample`

--- src/lib/constants.ts
+++ src/lib/constants.ts
@@ -1,6 +1,6 @@
 export const POOL_CONTRACT = "0xb27d23387324401f829c8c0b73a3df10a72c4080";
 export const BOND_MANAGER_CONTRACT = "0x60361121F74cE943D5cAFe0FBb98315A2eb2095b";
-export const INDEXER_URL = "https://dora-the-tipset-explorer.fly.dev/api";
+export const INDEXER_URL = process.env.INDEXER_URL
 
 export const NFT_BONDING_IMG_URL =
   "https://bafkreia2aki5tfngtc5wp6tbgzmynhz4wctcu3ns6stuspypd5eu3poxtm.ipfs.nftstorage.link/";
