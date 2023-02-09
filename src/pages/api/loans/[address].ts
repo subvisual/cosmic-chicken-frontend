54979caaeda8bd1abd98b79b16ac4b0677768752
Paula Carneiro <paulacarneiro215@gmail.com>
2023-02-09 16:36:04 +0000

    add expired loan

--- src/pages/api/loans/[address].ts
+++ src/pages/api/loans/[address].ts
@@ -12,5 +12,20 @@ export default async function handler(req: NextApiRequest, res: NextApiResponse<
   }
   const { data } = await response.json();
 
-  res.status(200).json(data);
+  const newData = [
+    ...data,
+    {
+      contract_address: "0xb27d23387324401f829c8c0b73a3df10a72c4080",
+      inserted_at: "2023-02-08T15:01:43",
+      projection_fields: {
+        owner: "0x0000000000000000000000000000000000000000",
+        repaid_amount: "5000000000000000000",
+        total_amount: "5000000000000000000",
+      },
+      projection_id: "0x7f1ed854f79g795f6d82764a6bae07693685de7c",
+      projection_type: "loan",
+    },
+  ];
+
+  res.status(200).json(newData);
 }
